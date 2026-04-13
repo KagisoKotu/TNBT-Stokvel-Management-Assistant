05e279ddc6ea84fcc3b6e1fd6f4548736f2c699d
KreepKid <thabisomahlangu909@gmail.com>
2026-04-13 05:36:56 +0200

    Update frontend dependencies and added Login component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+//don't forget to hide this in an .env file or somewhere in a real app! (and restrict the key in the google cloud console to only be valid on our domain, etc etc - security best practices)
+//want to use environment variables like v support- whatever that is
+const CLIENT_ID = "533738969001-3b55i33lkhl8adh3ofl4aicitq310eaq.apps.googleusercontent.com"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
