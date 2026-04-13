639d93be84d9fac038ab24959affbea55184070d
lisomketsu <lisomketsu04@gmail.com>
2026-04-13 10:58:12 +0200

    Update adminRoutes.js

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const { verifyToken, requireRole } = require('../middleware/authMiddleware');
 
 // checks if logged in, requireRole checks if they are an Admin
-router.get('/dashboard', verifyToken, requireRole('Admin'), (req, res) => {
+router.get('/dashboard', verifyToken, requireRole('admin'), (req, res) => {
   res.status(200).json({ message: 'Welcome to the Admin Dashboard' });
 });
 
 // Route: POST /api/admin/manage-users
-router.post('/manage-users', verifyToken, requireRole('Admin'), (req, res) => {
+router.post('/manage-users', verifyToken, requireRole('admin'), (req, res) => {
   // Logic for the Stokvel admin to manage members
   res.status(200).json({ message: 'User management endpoint accessed' });
 });
