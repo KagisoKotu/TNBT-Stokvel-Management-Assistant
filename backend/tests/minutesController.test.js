9e38e33c7c3dda96c32fee95c4c7605919e2c975
Gomolemo Rampa <wisemanrampa@gmail.com>
2026-04-24 23:37:56 +0200

    Fixed communication issues between database and minutesController.js

--- backend/tests/minutesController.test.js
+++ backend/tests/minutesController.test.js
@@ -1,7 +1,7 @@
 // tests/minutesController.test.js
 const { saveMinutes } = require('../controllers/minutesController');
 const Minutes = require('../models/minutes');
-// const User = require('../models/user'); // <-- Make sure you import User if you keep the block below!
+const User = require('../models/user'); // <-- Make sure you import User if you keep the block below!
 
 // Mock the Mongoose model
 jest.mock('../models/minutes');
